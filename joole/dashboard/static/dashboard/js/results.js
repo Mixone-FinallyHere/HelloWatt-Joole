307026bf5b28dc491ca4f5fe7aa55191378b7ef2
Miguel Terol Espino <miguel.terol.93@gmail.com>
2018-06-03 07:18:50 +0200

    Added all checks to Views. Improved html. Added morris js

--- joole/dashboard/static/dashboard/js/results.js
+++ joole/dashboard/static/dashboard/js/results.js
@@ -1,1 +1,68 @@
-// ----> YOUR CODE HERE <----
+$(window).on('load', function() {
+    
+    window.initgraphExpensesTot = function() {
+        // console.log("called total");
+        window.morrisGraphExpensesTot = new Morris.Bar({
+            element: 'expenses',
+            data: yearExpensesData,
+            xkey: 'year',
+            ykeys: ['expenses'],
+            labels: ['Dépenses'],
+            barRatio: 0.4,
+            xLabelAngle: 35,
+            hideHover: 'auto',
+            resize: 'true'
+        });
+    }
+    window.initgraphExpenses2017 = function() {
+        // console.log("called 2017");
+        window.morrisGraphExpenses2017 = new Morris.Bar({
+            element: 'expenses2017',
+            data: monthExpensesData2017,
+            xkey: 'month',
+            ykeys: ['expenses'],
+            labels: ['Dépenses'],
+            barRatio: 0.4,
+            xLabelAngle: 35,
+            hideHover: 'auto',
+            resize: 'true'
+        });
+    }
+    window.initgraphExpenses2016 = function() {
+        // console.log("called 2016");
+        window.morrisGraphExpenses2016 = new Morris.Bar({
+            element: 'expenses2016',
+            data: monthExpensesData2016,
+            xkey: 'month',
+            ykeys: ['expenses'],
+            labels: ['Dépenses'],
+            barRatio: 0.4,
+            xLabelAngle: 35,
+            hideHover: 'auto',
+            resize: 'true'
+        });
+    }
+    
+    $('#expensesTabs a').click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href');
+        var fun = "initgraph" + id.substr(1, id.length - 4);
+        var gen = "morrisGraph" + id.substr(1, id.length - 4);
+        // console.log(fun);    
+        $(this).tab('show');
+        if (typeof window[gen] === 'undefined') {        
+            window[fun]();
+        }
+    })
+    
+    initgraphExpenses2017();
+});
+
+// REPAIR ATTEMPT FOR BROKEN MORRIS DATA IN HIDDEN DIVS
+
+$('ul.nav a').on('shown.bs.tab', function (e) {
+    var id = $(this).attr('href');
+    var fun = "initgraph" + id.substr(1, id.length - 4);
+    var gen = "morrisGraph" + id.substr(1, id.length - 4);
+    window[gen].redraw();
+});
--- joole/dashboard/views.py
+++ joole/dashboard/views.py
@@ -4,11 +4,26 @@ from django.views.generic import View
 from .forms import ClientForm
 from .models import Conso_eur, Conso_watt
 
-
-ADMISSABLE_IDS = [0, 100] #  Eventually should be replaced by an sql query checking
+YEAR_LENGTH = 12          # Redundant but avoids magical numbers
+WINTER = [11, 0, 1, 2, 3] # December, January, February, and March
+                          # Lenght used to determine length of winter
+ADMISSABLE_IDS = [1, 100] #  Eventually should be replaced by an sql query checking
                           # table IDs lower and higher bounds assuming they are correctly
                           # modified on client creation / deletion.
-
+HEATING_THRESHOLD = 4  # Representing 25% 
+                       #  We will assume electrical heating if winter wattage average increase
+                       # is at least 25% higher than the wattage average of the rest of the year.
+                       # 25% chosen since winter does involve a general wattage increase due to
+                       # things like less sunlight, or more time spent at home because its cold.
+HEATING_MSGS = ["Unlikely", "Likely", "Highly likely"]
+DYSFUNCTION_THRESHOLD = 10  # Representing 10%
+                            #  We will assume wattage should more or less be the same year to year
+                            # hence a 10% increase will be noted as an anomaly.
+DYSFUNCTION_MSGS =  ["None", "Possible", "Detected"]    # Possible dysfunction check messages.
+                                                        # 0 = Nothing detected
+                                                        # 1 = Wattage change above threshold for 1 year
+                                                        # 2 = Wattage change above threshold for both years
+                                  
 class ClientFormView(View):
     def get(self, request):
         return render(request, 'dashboard/accueil.html')
@@ -27,31 +42,89 @@ class ClientFormView(View):
                 errorMsg = "Client ID must be a number."
             return render(request, 'dashboard/accueil.html', {"error":errorMsg})
 
-def results(request, client_id):    
-    
-    conso_euro = Conso_eur.objects.filter(client_id=client_id)
-    conso_watt = Conso_watt.objects.filter(client_id=client_id)
+def results(request, client_id):
+    euro_query = Conso_eur.objects.filter(client_id=client_id)
+    watt_query = Conso_watt.objects.filter(client_id=client_id) 
+    conso_euro = [monthFetcher(year) for year in euro_query]
+    conso_watt = [monthFetcher(year) for year in watt_query]  
     annual_costs = yearTotals(conso_euro)
-    is_elec_heating = True
-    dysfunction_detected = False     
-        
-    print(annual_costs)
-    
+    annual_wattage = yearTotals(conso_watt)
+    is_elec_heating = checkHeating(conso_watt)
+    dysfunction_detected = checkDysfunction(annual_wattage)  
+
     context = {
         "conso_euro": conso_euro,
         "conso_watt": conso_watt,
         "annual_costs": annual_costs,
         "is_elec_heating": is_elec_heating,
-        "dysfunction_detected": dysfunction_detected
+        "dysfunction_detected": dysfunction_detected,
+        "HEATING": HEATING_MSGS,
+        "DYSFUNCTION": DYSFUNCTION_MSGS
     }
     return render(request, 'dashboard/results.html', context)
+
+######### Check Functions ##########    
     
-def yearTotals(consumptionObjectList):
-    return [sum(monthFetcher(consumptionObjectList[0])), sum(monthFetcher(consumptionObjectList[1]))]
+def checkDysfunction(annual_wattage):
+    """ Helper function to check if possible dysfunction occurred. """  
+    dysfunction_detected = 0
+    anomalies = [False, False]
+    year_diff = normalizeValue(annual_wattage[0] - annual_wattage[1])
+    if year_diff > annual_wattage[0]/DYSFUNCTION_THRESHOLD:
+        anomalies[0] = True
+    if year_diff > annual_wattage[1]/DYSFUNCTION_THRESHOLD:
+        anomalies[1] = True
+    if all(anomalies):
+        dysfunction_detected = 2
+    elif any(anomalies):
+        dysfunction_detected = 1
+    # DEBUG
+    #print("Year difference watt :", annual_wattage[0], "-", annual_wattage[1], "=", normalizeValue(annual_wattage[0] - annual_wattage[1]))
+    #print("Dysfunction status :", DYSFUNCTION_MSGS[dysfunction_detected])
+    return dysfunction_detected
     
-def monthFetcher(consoObject):
-    return [consoObject.janvier, consoObject.fevrier, consoObject.mars, 
-            consoObject.avril, consoObject.mai, consoObject.juin,
-            consoObject.juillet, consoObject.aout, consoObject.septembre,
-            consoObject.octobre, consoObject.novembre, consoObject.decembre
-            ]
+def checkHeating(watt_year_list):
+    """ Helper function to check if possible electrical heating. """
+    is_elec_heating = 0
+    each_year = [False for i in range(len(watt_year_list))]
+    for i in range(len(watt_year_list)):
+        months = watt_year_list[i]
+        winter_average = sum([months[ind] for ind in WINTER]) / len(WINTER)
+        rest_of_year_average = sum(months[WINTER[-1]+1:WINTER[0]]) / (YEAR_LENGTH - len(WINTER))
+        # DEBUG
+        #print("Winter average       :", winter_average)
+        #print("Rest of year avergae :", rest_of_year_average)        
+        if normalizeValue(winter_average - rest_of_year_average) >= (rest_of_year_average/HEATING_THRESHOLD):
+            each_year[i] = True
+    if all(each_year):
+        is_elec_heating = 2
+    elif any(each_year):
+        is_elec_heating = 1
+    # DEBUG
+    #print("Electrical heating status :", HEATING_MSGS[is_elec_heating])
+    return is_elec_heating
+ 
+ 
+######### Helper Functions ##########
+     
+def yearTotals(conso_year_list):
+    """ 
+        Helper func to get year total.
+        Can be used AS IS if more years are added.
+    """
+    return [sum(year) for year in conso_year_list]
+    
+def monthFetcher(conso_object):
+    """ Fetch month data indepentent of actual model. """
+    return [conso_object.janvier, conso_object.fevrier, conso_object.mars, 
+            conso_object.avril, conso_object.mai, conso_object.juin,
+            conso_object.juillet, conso_object.aout, conso_object.septembre,
+            conso_object.octobre, conso_object.novembre, conso_object.decembre
+            ] 
+            
+def normalizeValue(possibleNegative):
+    """ 
+        Turn positive in case a result is negative.
+        Unexpected but could happen. 
+    """
+    return ((possibleNegative)**2)**(1/2)
